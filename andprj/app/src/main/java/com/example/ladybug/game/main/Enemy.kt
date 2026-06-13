fe2b0c6d2f5281d531e4ab96f869ff31fdaa17f6
youngkyu12 <112385423+youngkyu12@users.noreply.github.com>
2026-06-13 11:58:48 +0900

    Enemy 시작 위치를 화면 위 바깥으로 조정 및 화면 밖으로 나가면 삭제되도록 정리

--- andprj/app/src/main/java/com/example/ladybug/game/main/Enemy.kt
+++ andprj/app/src/main/java/com/example/ladybug/game/main/Enemy.kt
@@ -3,19 +3,26 @@ package com.example.ladybug.game.main
 import com.example.ladybug.R
 import kr.ac.tukorea.ge.spgp2026.a2dg.objects.Sprite
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import kotlin.collections.remove
 
 class Enemy(gctx: GameContext, x: Float, y: Float) : Sprite(gctx, R.mipmap.enemy) {
     override var width = ENEMY_WIDTH
     override var height = ENEMY_HEIGHT
     override var x = x
-    override var y = y
+    override var y = -ENEMY_HEIGHT / 2f
 
     init {
         syncDstRect()
     }
 
+
     override fun update(gctx: GameContext) {
         y += SPEED * gctx.frameTime
+
+        if (y - height / 2f > gctx.metrics.height) {
+            val scene = gctx.scene as? MainScene ?: return
+            scene.world.remove(this, MainScene.Layer.ENEMY)
+        }
         syncDstRect()
     }
 
